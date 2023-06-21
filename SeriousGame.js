ec4abf7b581642b4ef06f7a86544442e895df610
Vanelslander Gérald <g.vanelslander@ludus-academie.com>
2023-06-21 20:49:31 +0200

    modification

--- SeriousGame.js
+++ SeriousGame.js
@@ -1,134 +1,6 @@
-var w = 500;
-var h = 500;
-var n = 50;
-var s = w / n;
-var player = {
-    parts: [
-        { X: 1, Y: 0 },
-        { X: 0, Y: 0 }
-    ]
-}
-var fps = 10;
-var pb = 1;
-var min = 0;
-var max = n - 1;
-var score = 0;
-var candy = {
-    X: Math.round(Math.random() * max),
-    Y: Math.round(Math.random() * max)
-}
 var can = document.getElementById("First");
-can.width = w;
-can.height = h;
-var ref;
-//var ctx = can.getContext("2d");
-var speed = {
-    X: 1,
-    Y: 0
-}
-setRate(1000);
-//setInterval(update,1000/fps);
-//QUEL DIRECTION?
-document.addEventListener("keydown", f_keydown)
-function f_keydown(e) {
-    switch (e.keyCode) {
-        case 37: //GAUCHE
-            speed.X = -1;
-            speed.Y = 0;
-            break;
-        case 38: //HAUT
-            speed.Y = -1;
-            speed.X = 0;
-            break;
-        case 39: //DROITE
-            speed.X = 1;
-            speed.Y = 0;
-            break;
-        case 40: //BAS
-            speed.Y = 1;
-            speed.X = 0;
-            break;
-    }
-}
-
-function update() {
-
-    player.parts.unshift({
-        X: player.parts[0].X + speed.X * pb,
-        Y: player.parts[0].Y + speed.Y * pb
-    })
-    ctx.clearRect(0, 0, w, h);
-    //INIT FOOD
-    ctx.fillStyle = "rgb(255,0,0)";
-    ctx.fillRect(candy.X * s, candy.Y * s, s, s);
-    //INIT SERPENT
-    ctx.fillStyle = "rgb(0,255,0)";
-    var n_parts = player.parts.length;
-    for (var i = 0; i < n_parts; i++) {
-        var part = player.parts[i];
-        ctx.fillRect(part.X * s, part.Y * s, s, s);
-    };
-    //TELEPORTATION
-    if (player.parts[0].X >= n) {
-        player.parts[0].X = 0;
-    }
-    if (player.parts[0].X < 0) {
-        player.parts[0].X = n;
-    }
-    if (player.parts[0].Y >= n) {
-        player.parts[0].Y = 0;
-    }
-    if (player.parts[0].Y < 0) {
-        player.parts[0].Y = n;
-    }
-    //MIAM MIAM MIAM
-    if ((candy.X == player.parts[0].X) && (candy.Y == player.parts[0].Y)) {
-        console.log("MIAM MiAM");
-        candy = {
-            X: Math.round(Math.random() * max),
-            Y: Math.round(Math.random() * max)
-        }
-        score = score + 50;
-        fps++;
-        console.log(fps);
-        console.log(score);
-    }
-    else {
-        player.parts.pop();
-    }
-    //PERDU?
-    for (var i = 1; i < player.parts.length; i++) {
-        if ((player.parts[0].X == player.parts[i].X) && (player.parts[0].Y == player.parts[i].Y)) {
-            console.log(player.parts[0]);
-            console.log("PERDU");
-            alert("You loose");
-        }
-    }
-    //GRILLE
-    /*for(var i=0; i<n; i++){
-
-        for(var j=0; j<n; j++) {
-
-            ctx.beginPath();
-            ctx.strokeStyle = "rgb(0,0,255)";
-            ctx.moveTo (i*s,j*s);
-            ctx.lineTo (i*s,s*(j+1));
-            ctx.lineTo (s*(i+1),s*(j+1));
-            ctx.lineTo (s*(i+1),s*j);
-            ctx.lineTo (i*s,s*j);
-            ctx.closePath();
-            ctx.stroke();
- 
-        }
-    }*/
-}
-function setRate(rate) {
-    clearInterval(ref);
-    ref = setInterval(update, rate / fps);
-}
-
 //Changement de page
-const levelmax = 15;
+const levelmax = 25;
 let curentlevel = 1;
 let succeslevel = 1;
 let previous = document.querySelector("#PreviousPage");
@@ -612,8 +484,348 @@ verif6.addEventListener("click", () => {
         total = 0;
     }
     else {
+        succeslevel = 12;
+    }
+    console.log(total);
+    appearnextbutton();
+})
+
+//Snake creation de la carte
+let verif7 = document.querySelector("#Validation7");
+let input37 = document.querySelector("#input37");
+let input38 = document.querySelector("#input38");
+let input39 = document.querySelector("#input39");
+let input40 = document.querySelector("#input40");
+let input41 = document.querySelector("#input41");
+let input42 = document.querySelector("#input42");
+let input43 = document.querySelector("#input43");
+
+verif7.addEventListener("click", () => {
+    total = 0;
+
+    if (input37.value.toUpperCase() == "ENTIER") {
+        total = total + 1;
+        input37.style.background = "green";
+    }
+    else {
+        input37.style.background = "red";
+    }
+
+    if (input38.value.toUpperCase() == "POUR") {
+        total = total + 1;
+        input38.style.background = "green";
+    }
+    else {
+        input38.style.background = "red";
+    }
+
+    if (input39.value.toUpperCase() == "A") {
+        total = total + 1;
+        input39.style.background = "green";
+    }
+    else {
+        input39.style.background = "red";
+    }
+
+    if (input40.value.toUpperCase() == "DE") {
+        total = total + 1;
+        input40.style.background = "green";
+    }
+    else {
+
+        input40.style.background = "red";
+    }
+
+    if (input41.value.toUpperCase() == "FAIRE") {
+        total = total + 1;
+        input41.style.background = "green";
+    }
+    else {
+        input41.style.background = "red";
+    }
+
+    if (input42.value.toLowerCase() == "map") {
+        total = total + 1;
+        input42.style.background = "green";
+    }
+    else {
+        input42.style.background = "red";
+    }
+
+    if (input43.value.toUpperCase() == "FINPOUR") {
+        total = total + 1;
+        input43.style.background = "green";
+    }
+    else {
+        input43.style.background = "red";
+    }
+
+    if (total != 7) {
+        total = 0;
+    }
+    else {
         succeslevel = 13;
     }
     console.log(total);
     appearnextbutton();
 })
+
+//Affichage de la map
+let verif8 = document.querySelector("#Validation8");
+let input44 = document.querySelector("#input44");
+let input45 = document.querySelector("#input45");
+let input46 = document.querySelector("#input46");
+let input47 = document.querySelector("#input47");
+let input48 = document.querySelector("#input48");
+let input49 = document.querySelector("#input49");
+let input50 = document.querySelector("#input50");
+let input51 = document.querySelector("#input51");
+let input52 = document.querySelector("#input52");
+let input53 = document.querySelector("#input53");
+let planquer = document.querySelector(".planquer");
+
+verif8.addEventListener("click", () => {
+    total = 0;
+
+    if (input44.value.toUpperCase() == "TABLEAU") {
+        total = total + 1;
+        input44.style.background = "green";
+    }
+    else {
+        input44.style.background = "red";
+    }
+
+    if (input45.value.toUpperCase() == "POUR") {
+        total = total + 1;
+        input45.style.background = "green";
+    }
+    else {
+        input45.style.background = "red";
+    }
+
+    if (input46.value.toUpperCase() == "DE") {
+        total = total + 1;
+        input46.style.background = "green";
+    }
+    else {
+        input46.style.background = "red";
+    }
+
+    if (input47.value.toUpperCase() == "A") {
+        total = total + 1;
+        input47.style.background = "green";
+    }
+    else {
+        input47.style.background = "red";
+    }
+
+    if (input48.value.toUpperCase() == "FAIRE") {
+        total = total + 1;
+        input48.style.background = "green";
+    }
+    else {
+
+        input48.style.background = "red";
+    }
+
+    if (input49.value.toUpperCase() == "POUR") {
+        total = total + 1;
+        input49.style.background = "green";
+    }
+    else {
+        input49.style.background = "red";
+    }
+
+    if (input50.value.toUpperCase() == "DE") {
+        total = total + 1;
+        input50.style.background = "green";
+    }
+    else {
+        input50.style.background = "red";
+    }
+
+    if (input51.value.toUpperCase() == "A") {
+        total = total + 1;
+        input51.style.background = "green";
+    }
+    else {
+        input51.style.background = "red";
+    }
+
+    if (input52.value.toUpperCase() == "FAIRE") {
+        total = total + 1;
+        input52.style.background = "green";
+    }
+    else {
+        input52.style.background = "red";
+    }
+
+    if (input53.value.toUpperCase() == "ECRIREALALIGNE") {
+        total = total + 1;
+        input53.style.background = "green";
+    }
+    else {
+        input53.style.background = "red";
+    }
+
+    if (total != 10) {
+        total = 0;
+    }
+    else {
+        succeslevel = 14;
+        planquer.classList.remove("planquer");
+    }
+    console.log(total);
+    appearnextbutton();
+})
+
+var partie = 0;
+
+//Le snake
+var w = 500;
+var h = 500;
+var n = 50;
+var s = w / n;
+var player = {
+    parts: [
+        { X: 1, Y: 0 },
+        { X: 0, Y: 0 }
+    ]
+}
+var fps = 10;
+var pb = 1;
+var min = 0;
+var max = n - 1;
+var score = 0;
+var candy = {
+    X: Math.round(Math.random() * max),
+    Y: Math.round(Math.random() * max)
+}
+can.width = w;
+can.height = h;
+var ref;
+var ctx = can.getContext("2d");
+var speed = {
+    X: 1,
+    Y: 0
+}
+setRate(1000);
+//setInterval(update,1000/fps);
+//QUEL DIRECTION?
+document.addEventListener("keydown", f_keydown)
+function f_keydown(e) {
+    switch (e.keyCode) {
+        case 37: //GAUCHE
+            speed.X = -1;
+            speed.Y = 0;
+            break;
+        case 38: //HAUT
+            speed.Y = -1;
+            speed.X = 0;
+            break;
+        case 39: //DROITE
+            speed.X = 1;
+            speed.Y = 0;
+            break;
+        case 40: //BAS
+            speed.Y = 1;
+            speed.X = 0;
+            break;
+    }
+}
+function initfood(){
+    ctx.fillStyle = "rgb(255,0,0)";
+    ctx.fillRect(candy.X * s, candy.Y * s, s, s); 
+}
+
+function initserpent(){
+    ctx.fillStyle = "rgb(0,255,0)";
+    var n_parts = player.parts.length;
+    for (var i = 0; i < n_parts; i++) {
+        var part = player.parts[i];
+        ctx.fillRect(part.X * s, part.Y * s, s, s);
+    };
+}
+
+function teleportation(){
+    if (player.parts[0].X >= n) {
+        player.parts[0].X = 0;
+    }
+    if (player.parts[0].X < 0) {
+        player.parts[0].X = n;
+    }
+    if (player.parts[0].Y >= n) {
+        player.parts[0].Y = 0;
+    }
+    if (player.parts[0].Y < 0) {
+        player.parts[0].Y = n;
+    }
+}
+
+function Miam(){
+    if ((candy.X == player.parts[0].X) && (candy.Y == player.parts[0].Y)) {
+        console.log("MIAM MiAM");
+        candy = {
+            X: Math.round(Math.random() * max),
+            Y: Math.round(Math.random() * max)
+        }
+        score = score + 50;
+        fps++;
+        console.log(fps);
+        console.log(score);
+    }
+    else {
+        player.parts.pop();
+    }
+}
+
+function loose(){
+    for (var i = 1; i < player.parts.length; i++) {
+        if ((player.parts[0].X == player.parts[i].X) && (player.parts[0].Y == player.parts[i].Y)) {
+            console.log(player.parts[0]);
+            console.log("PERDU");
+            alert("You loose");
+        }
+    }
+}
+function move(){
+    player.parts.unshift({
+        X: player.parts[0].X + speed.X * pb,
+        Y: player.parts[0].Y + speed.Y * pb
+    })
+}
+function update() {
+
+    ctx.clearRect(0, 0, w, h);
+    if (partie >= 2){
+        move();
+        if (partie < 5){
+            player.parts.pop();
+        }
+    }
+    
+    //INIT FOOD
+    if (partie >= 4){
+        initfood();
+    }
+    //INIT SERPENT
+    if (partie >= 1){
+        initserpent();
+    }
+    //TELEPORTATION
+    if (partie >= 3){
+        teleportation();
+    }
+    //MIAM MIAM MIAM
+    if(partie >= 5){
+        Miam();
+    }
+    //PERDU?
+    if(partie >= 6){
+        loose();
+    }
+}
+function setRate(rate) {
+    clearInterval(ref);
+    ref = setInterval(update, rate / fps);
+}
